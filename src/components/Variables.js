e0357fefc22aa76bffa4b489fe037f7769b81b4c
Narendra Syahrasyad <nanda.s@hey.com>
2020-12-16 13:35:31 -0800

    feat: slight layout animation changes

--- src/components/Variables.js
+++ src/components/Variables.js
@@ -9,21 +9,22 @@ export default function Variables({ params, vars = {}, prev }) {
   const localVars = values.filter(([key]) => !params.includes(key));
   return (
     <AnimateSharedLayout>
-      <section style={{ maxWidth: "40rem" }} className="font-mono w-full">
+      <section
+        style={{ maxWidth: "40rem", maxHeight: "75%" }}
+        className="font-mono w-full overflow-y-scroll"
+      >
         <motion.h1 layout className="text-center mb-4">
           debugger @ line {vars.line}
         </motion.h1>
         <ul className="w-full">
-          <AnimatePresence>
-            <li className="mb-4">
-              Parameters
-              <VariableList vars={parameters} prev={prev} />
-            </li>
-            <li>
-              Local Variables
-              <VariableList vars={localVars} prev={prev} />
-            </li>
-          </AnimatePresence>
+          <motion.li layout className="mb-4">
+            <motion.span layout>Parameters</motion.span>
+            <VariableList vars={parameters} prev={prev} />
+          </motion.li>
+          <motion.li layout>
+            <motion.span layout>Local Variables</motion.span>
+            <VariableList vars={localVars} prev={prev} />
+          </motion.li>
         </ul>
       </section>
     </AnimateSharedLayout>
@@ -32,27 +33,29 @@ export default function Variables({ params, vars = {}, prev }) {
 
 function VariableList({ vars, prev }) {
   return (
-    <ul className="mt-2">
-      {vars.map(([key, val]) => {
-        const hasLast = prev && prev.hasOwnProperty(key);
-        return (
-          <motion.li
-            key={key}
-            layout
-            className="flex w-full mb-2 break-words"
-            initial={{ opacity: 0, y: 10 }}
-            animate={{
-              opacity: hasLast && equal(prev[key], val) ? 0.2 : 1,
-              y: 0,
-            }}
-            exit={{ opacity: 0, y: 10 }}
-          >
-            <VariableItem className="w-1/3 mr-2">{key}</VariableItem>
-            <VariableItem className="w-2/3">{JSON.stringify(val)}</VariableItem>
-          </motion.li>
-        );
-      })}
-    </ul>
+    <motion.ul layout className="mt-2">
+      <AnimatePresence>
+        {vars.map(([key, val]) => {
+          const hasLast = prev && prev.hasOwnProperty(key);
+          return (
+            <motion.li
+              key={key}
+              layout
+              className="flex w-full mb-2 break-words"
+              initial={{ opacity: 0, y: 10 }}
+              animate={{
+                opacity: hasLast && equal(prev[key], val) ? 0.2 : 1,
+                y: 0,
+              }}
+              exit={{ opacity: 0, y: 10 }}
+            >
+              <VariableItem className="w-1/3 mr-2">{key}</VariableItem>
+              <VariableItem className="w-2/3">{JSON.stringify(val)}</VariableItem>
+            </motion.li>
+          );
+        })}
+      </AnimatePresence>
+    </motion.ul>
   );
 }
 
